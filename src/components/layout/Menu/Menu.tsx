5053b71f71c19d6bc0341413485cc2cf02874b65
Jakub Was <wasjakub.jw@gmail.com>
2021-11-07 21:00:51 +0100

    refactor: :recycle: Working on basic layout structure #3

--- src/components/layout/Menu/Menu.tsx
+++ src/components/layout/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import WorldMapSVG from "../../maps/WorldMapSVG";
 import MenuButton from "../../buttons/MenuButton/MenuButton";
@@ -8,43 +8,67 @@ import {
   StyledSelectMode,
 } from "./Menu.styles";
 
-const Menu = () => (
-  <StyledMenuContainer>
-    <WorldMapSVG />
-    <StyledSelectMode>Select Mode</StyledSelectMode>
-    <StyledMenuList>
-      <MenuButton>
-        <Link to="/USA" className="link">
-          US states
-        </Link>
-      </MenuButton>
-      <MenuButton>
-        <Link to="/NorthAmerica" className="link">
-          North America
-        </Link>
-      </MenuButton>
-      <MenuButton>
-        <Link to="/SouthAmerica" className="link">
-          South America
-        </Link>
-      </MenuButton>
-      <MenuButton>
-        <Link to="/Europe" className="link">
-          Europe
-        </Link>
-      </MenuButton>
-      <MenuButton>
-        <Link to="/Africa" className="link">
-          Africa
-        </Link>
-      </MenuButton>
-      <MenuButton>
-        <Link to="/Asia" className="link">
-          Asia
-        </Link>
-      </MenuButton>
-    </StyledMenuList>
-  </StyledMenuContainer>
-);
+const Menu = () => {
+  const [mode, setMode] = useState("");
+
+  return (
+    <StyledMenuContainer>
+      <WorldMapSVG />
+      <StyledSelectMode>
+        {mode === "countries" ? "Select Region" : "Select Mode"}
+      </StyledSelectMode>
+      <StyledMenuList>
+        {mode !== "countries" ? (
+          <>
+            <MenuButton>
+              <Link to="/USA" className="link">
+                US states
+              </Link>
+            </MenuButton>
+            <MenuButton>
+              <Link
+                to="/"
+                className="link"
+                onClick={() => {
+                  setMode("countries");
+                }}
+              >
+                Countries
+              </Link>
+            </MenuButton>
+          </>
+        ) : (
+          <>
+            <MenuButton>
+              <Link to="/NorthAmerica" className="link">
+                North America
+              </Link>
+            </MenuButton>
+            <MenuButton>
+              <Link to="/SouthAmerica" className="link">
+                South America
+              </Link>
+            </MenuButton>
+            <MenuButton>
+              <Link to="/Europe" className="link">
+                Europe
+              </Link>
+            </MenuButton>
+            <MenuButton>
+              <Link to="/Africa" className="link">
+                Africa
+              </Link>
+            </MenuButton>
+            <MenuButton>
+              <Link to="/Asia" className="link">
+                Asia
+              </Link>
+            </MenuButton>
+          </>
+        )}
+      </StyledMenuList>
+    </StyledMenuContainer>
+  );
+};
 
 export default Menu;
